c07a699a88fd934481480394fe73fda85eddeac9
Steven Nersesian <79111668+StoveN64@users.noreply.github.com>
2023-01-06 05:34:09 -0700

    Cleaned up Auth, CSS animations completed

---
+++ README.md
@@ -0,0 +1,16 @@
+Description:
+Jumplist was orignially thoguht up as a way for my friend and I to catalog co-op video games to play. We had such a growing list it became hard to sort through our Steam wishlists. This website was orignally for co-op videeo games, but it can be used for anything you want to track and complete. It was also built as my Dev-Moutain Capstone project. Anything you want to write down and complete, Jumplist can help you!
+
+Technologies used:
+Node.js, Express, Express Sessions, Node.js, MySql, Javascript,HTML/CSS.
+
+How to install:
+Run server using,
+
+`node login.js`
+
+open website using,
+
+`localhost:4000`
+
+Version 1.0
---
+++ index.js
@@ -0,0 +1,49 @@
+document.querySelector("form").addEventListener("submit", addGame);
+const message = document.querySelector('#message')
+function addGame(event) {
+  event.preventDefault();
+  let inputField = document.querySelector('input')
+  const game = document.createElement('li')
+  const gameTitle = document.createElement("span");
+  gameTitle.textContent = inputField.value;
+  gameTitle.addEventListener("click", completeGame);
+  game.appendChild(gameTitle)
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "Delete";
+  deleteBtn.addEventListener("click", deleteGame);
+  game.appendChild(deleteBtn);
+
+  const list = document.querySelector("ul");
+  list.appendChild(game);
+  inputField.value = ''
+}
+
+function deleteGame(event) {
+    message.textContent = `${event.target.parentNode.firstChild.textContent} deleted!`
+    
+    message.textContent = `Game deleted!`
+
+    revealMessage()
+
+    event.target.parentNode.remove();
+}
+
+function completeGame(event) {
+    event.target.classList.toggle('checked')
+
+    if (event.target.classList.contains('checked') === true) {        
+        message.textContent = `You completed ${event.target.textContent}, great job!`
+    } else {
+        message.textContent = `${event.target.textContent} added back!`
+    }
+
+    revealMessage()
+}
+
+function revealMessage() {
+    
+    setTimeout(() => {
+        message.classList.add('hide')
+    }, 1000)
+}
